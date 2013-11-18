5bbb0aa0918d50f6eaade41e43d2337b42087a8d
Trevor Toryk <TrevT93@gmail.com>
2013-11-18 15:20:03 -0500

    App Bridge +get_reservation +modify_reservation
    Added the 2 functions described (should test).  get_reservation may need
    to be changed if we go to XML.  Commented out include algorithm.php,
    it's not in use and it's generating junk text right now.  Added
    commenting.  Removed a comment in the pagr_db.php file.

--- php/application_bridge.php
+++ php/application_bridge.php
@@ -40,32 +40,31 @@
 	// in this namespace (on this page).
 	namespace pagr\app_bridge;
 	include 'pagr_db.php';
+//	include 'seating-algorithm/algorithm.php'
 ?>
 <?php
 	/**
 	 * Creates a new reservation given $_REQUEST['handset_id'].
 	 * 
-	 * @return Integer A positive integer representing the reservation ID if successful, ERROR if not.
+	 * @return Integer A positive integer representing the reservation_id if successful, ERROR if not.
 	 */
 	function create_reservation() {
-		// PRECONDITION: handset_id, party_size must be specified
+		// PRECONDITION: handset_id, party_size, patron_name must be specified
 		if (!isset($_REQUEST['handset_id'])) die("ERROR: handset_id required");
 		if (!isset($_REQUEST['party_size'])) die("ERROR: party_size required");
 		if (!isset($_REQUEST['patron_name'])) die("ERROR: patron_name required");
 		
-		$PATRON_NAME = $_REQUEST['patron_name'];
-		$PARTY_SIZE = $_REQUEST['party_size'];
-		
-		$RESERVATION_TIME = NULL;
-		
 		// OPTIONAL PRECONDITION: reservation_time specifies a reservation, not a walk-in.
 		// NOTE: reservation_time MUST be in the following format: "YYYY-MM-DD HH:MM:SS"
 		//	(checking this will be reserved to the database though, as it is more efficient)
+		$RESERVATION_TIME = NULL;
 		if (isset($_REQUEST['reservation_time'])) $RESERVATION_TIME = $_REQUEST['reservation_time'];
 		
 		// Connect to the database, set the local variables
 		$DB = get_pagr_db_connection();
-		$HANDSET_ID = $_REQUEST['handset_id'];
+		$HANDSET_ID = (int)$_REQUEST['handset_id'];
+		$PATRON_NAME = $_REQUEST['patron_name'];
+		$PARTY_SIZE = (int)$_REQUEST['party_size'];
 		
 		// Simple injection attack prevention; by removing a semicolon, you can
 		// prevent a SQL injection attack by creating a string that is not
@@ -73,25 +72,33 @@
 		$HANDSET_ID = str_replace(";", "", $HANDSET_ID);
 		if (!empty($RESERVATION_TIME)) $RESERVATION_TIME = str_replace(";", "", $RESERVATION_TIME);
 		
+		// Run the query and fetch the results
 		$RESULT = $DB->query("SELECT count(*) FROM patrons_t WHERE android_id = '$HANDSET_ID'");
 		$EXISTS = $RESULT->fetch_row()[0];
+		
+		// If the one result (which is a count(*)) is 0 and a $RESERVATION_TIME has been specified, insert a new reservation.
 		if ($EXISTS == 0 && !empty($RESERVATION_TIME)) {
 			$RESULT = $DB->query("INSERT INTO patrons_t(name, party_size, reservation_time, android_id) VALUES ('$PATRON_NAME', $PARTY_SIZE, '$RESERVATION_TIME', '$HANDSET_ID';)");
 			
 			if ($RESULT === false) {
-				die("ERROR: unspecified database error");
+				$ERROR = $DB->error;
+				echo "ERROR: $ERROR";
 			}
 			
+			// ...and then return the result.
 			$RESULT = $DB->query("SELECT patron_id FROM patrons_t WHERE android_id = '$HANDSET_ID';");
 			echo $RESULT->fetch_row()[0];
 		}
+		// If the one result is 0 and no $RESERVATION_TIME, then it's just a walk-in customer.
 		else if ($EXISTS == 0) {
 			$RESULT = $DB->query("INSERT INTO patrons_t(name, party_size, android_id) VALUES ('$PATRON_NAME', $PARTY_SIZE, '$HANDSET_ID');");
 			
 			if ($RESULT === false) {
-				die("ERROR: unspecified database error");
+				$ERROR = $DB->error;
+				echo "ERROR: $ERROR";
 			}
 			
+			// Return the walk-in's ID number.
 			$RESULT = $DB->query("SELECT patron_id FROM patrons_t WHERE android_id = '$HANDSET_ID';");
 			echo $RESULT->fetch_row()[0];
 		}
@@ -101,17 +108,41 @@
 	/**
 	 * Prints reservation details in a <property>:<value>; format.  Requires $_REQUEST['reservation_id'].
 	 * 
-	 * @return String A long string about reservation details on multiple lines, or -2 if the reservation doesn't exist.
+	 * @return String A long string about reservation details, or ERROR if the reservation doesn't exist.
 	 */
     function get_reservation() {
-        echo "get a reservation!";
+        // PRECONDITION: handset_id, reservation_id must be specified
+		if (!isset($_REQUEST['handset_id'])) die("ERROR: handset_id required");
+		if (!isset($_REQUEST['reservation_id'])) die("ERROR: reservation_id required");
 		
-		// This function gets reservation details for a given reservation_id.
-		// if (exists(reservation_id))
-		//		get reservation_details from DB;
-		//		return reservation_details;
-		// else
-		//		return -2;
+		// Connect to the database, set the local variables
+		$DB = get_pagr_db_connection();
+		$HANDSET_ID = $_REQUEST['handset_id'];
+		$PATRON_ID = (int)$_REQUEST['reservation_id'];
+		
+		// Simple injection attack prevention; by removing a semicolon, you can
+		// prevent a SQL injection attack by creating a string that is not
+		// SQL-compliant and will force a MySQL error.
+		$HANDSET_ID = str_replace(";", "", $HANDSET_ID);
+		
+		// Run the query, return the result
+		$RESULT = $DB->query("SELECT name, party_size, reservation_time FROM patrons_t WHERE android_id = '$HANDSET_ID' AND patron_id = $PATRON_ID LIMIT 1;");
+		if ($RESULT === false) {
+			$ERROR = $DB->error;
+			echo "ERROR: $ERROR";
+		}
+		else {
+			$ROW = $RESULT->fetch_row();
+			if (empty($ROW)) die("ERROR: no reservation found");
+			$NAME = $ROW[0];
+			$PARTY = $ROW[1];
+			$RESERVATION = $ROW[2];
+			if (!empty($RESERVATION)) echo "name=$NAME;party_size=$PARTY;reservation_time=$RESERVATION";
+			else echo "name=$NAME;party_size=$PARTY;";
+		}
+		
+		// Close the connection.
+		$DB->close();
 	}
 	
 	/**
@@ -121,27 +152,67 @@
 	 * @return Boolean True if the reservation time change was successful, -3 if not.
 	 */
 	function modify_reservation() {
-		echo "mod a reservation!";
+		// PRECONDITION: handset_id, reservation_id must be specified
+		if (!isset($_REQUEST['handset_id'])) die("ERROR: handset_id required");
+		if (!isset($_REQUEST['reservation_id'])) die("ERROR: reservation_id required");
 		
-		// This function allows you to modify a reservation time.
-		// if (exists(reservation_id) and exists(reservation_new_time))
-		//		set reservation_time to DB;
-		//		return true;
-		// else
-		//		return -3;
+		// OPTIONAL PRECONDIITON: party_size, reservation_time
+		$PARTY_SIZE = NULL;
+		$RESERVATION_TIME = NULL;
+		if (isset($_REQUEST['party_size'])) $PARTY_SIZE = (int)$_REQUEST['party_size'];
+		if (isset($_REQUEST['reservation_time'])) $RESERVATION_TIME = $_REQUEST['reservation_time'];
+		
+		// What if neither of them are specified?
+		if (empty($PARTY_SIZE) && empty($RESERVATION_TIME)) die("ERROR: nothing to change");
+		
+		// Connect to the database, set the local variables
+		$DB = get_pagr_db_connection();
+		$HANDSET_ID = $_REQUEST['handset_id'];
+		$PATRON_ID = (int)$_REQUEST['reservation_id'];
+		$RESULT = NULL;
+		
+		// Simple injection attack prevention; by removing a semicolon, you can
+		// prevent a SQL injection attack by creating a string that is not
+		// SQL-compliant and will force a MySQL error.
+		$HANDSET_ID = str_replace(";", "", $HANDSET_ID);
+		
+		// Both changed
+		if (!empty($PARTY_SIZE) && !empty($RESERVATION_TIME)) {
+			$RESULT = $DB->query("UPDATE patrons_t SET party_size = $PARTY_SIZE AND reservation_time = $RESERVATION_TIME WHERE android_id = '$HANDSET_ID' AND patron_id = $PATRON_ID;");
+		}
+		// Only the party size changed
+		elseif (!empty($PARTY_SIZE)) {
+			$RESULT = $DB->query("UPDATE patrons_t SET party_size = $PARTY_SIZE WHERE android_id = '$HANDSET_ID' AND patron_id = $PATRON_ID;");
+		}
+		// Only the reservation time changed
+		elseif (!empty($RESERVATION_TIME)) {
+			$RESULT = $DB->query("UPDATE patrons_t SET reservation_time = $RESERVATION_TIME WHERE android_id = '$HANDSET_ID' AND patron_id = $PATRON_ID;");
+		}
+		
+		// Run the query, return the result
+		if ($RESULT === false) {
+			$ERROR = $DB->error;
+			echo "ERROR: $ERROR";
+		}
+		else {
+			echo "OK";
+		}
+		
+		// Close the connection.
+		$DB->close();
 	}
 	
 	/**
-	 * 
+	 * This function has no arguments.  Instead, it merely returns the current wait time for a table.
 	 */
 	function check_wait_time() {
-		
+		// This is just a call to a function in seating-algorithm/algorithm.php
 	}
 	
 	/**
 	 * 
 	 */
-	function create_update_order() {
+	function make_order() {
 		echo "create an order!";
 		
 		// This function allows you to create a new order for a reservation (or
@@ -154,6 +225,13 @@
 		//		return new_order_id;
 		// else
 		//		return -4;
+	}
+	
+	/**
+	 * 
+	 */
+	function add_change_order_item() {
+		
 	}
 	
 	/**
@@ -200,7 +278,7 @@
 		// Connect to the database, set the local variables
 		$DB = get_pagr_db_connection();
 		$HANDSET_ID = $_REQUEST['handset_id'];
-		$PATRON_ID = $_REQUEST['reservation_id'];
+		$PATRON_ID = (int)$_REQUEST['reservation_id'];
 		
 		// Simple injection attack prevention; by removing a semicolon, you can
 		// prevent a SQL injection attack by creating a string that is not
@@ -224,10 +302,6 @@
 	/**
 	 * This function acknowledges a page request.
 	 * 
-	 * Request variables to be set:
-	 *		handset_id: the handset ID of the mobile device.
-	 *		reservation_id: the ID of the patron's reservation.
-	 * 
 	 * Both must be set to acknowledge! (that way it's much harder to falsify an
 	 * acknowledgement).
 	 * 
@@ -241,7 +315,7 @@
 		// Connect to the database, set the local variables
 		$DB = get_pagr_db_connection();
 		$HANDSET_ID = $_REQUEST['handset_id'];
-		$PATRON_ID = $_REQUEST['reservation_id'];
+		$PATRON_ID = (int)$_REQUEST['reservation_id'];
 		
 		// Simple injection attack prevention; by removing a semicolon, you can
 		// prevent a SQL injection attack by creating a string that is not
